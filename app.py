df1a4fdde00dc7d8a34ffb69a9c3f9fc3ea292f9
Nancimento <revandoaruperes6@gmail.com>
2025-10-21 07:57:53 +0800

    modif jadi presensi

--- app.py
+++ app.py
@@ -1,29 +1,38 @@
 from flask import Flask, request, render_template, redirect
 from pymongo import MongoClient
+from datetime import datetime
 
 app = Flask(__name__)
 
-# Ambil URL dari environment variable
 mongo_uri = "mongodb+srv://lionking:lionking123@revando.zqfyyjo.mongodb.net/?retryWrites=true&w=majority&appName=Revando"
 
-# Koneksi ke MongoDB Atlas
 client = MongoClient(mongo_uri)
 db = client['lionking']
-collection = db['lion']
+collection = db['attendance']
 
 @app.route('/')
 def index():
-    users = list(collection.find())
-    return render_template('index.html', users=users)
+    attendances = list(collection.find().sort('timestamp', -1))
+    return render_template('index.html', attendances=attendances)
 
 @app.route('/add', methods=['POST'])
-def add_user():
+def add_attendance():
     name = request.form['name']
-    collection.insert_one({'name': name})
+    status = request.form['status']
+    notes = request.form['notes']
+    timestamp = datetime.now()
+    
+    collection.insert_one({
+        'name': name,
+        'status': status,
+        'notes': notes,
+        'timestamp': timestamp,
+        'date': timestamp.strftime('%Y-%m-%d')
+    })
     return redirect('/')
 
 @app.route('/delete/<id>')
-def delete_user(id):
+def delete_attendance(id):
     from bson import ObjectId
     collection.delete_one({'_id': ObjectId(id)})
     return redirect('/')
